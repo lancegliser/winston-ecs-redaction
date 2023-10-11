4f157ea6269f310df1491a6df7dfe331817bd425
Lance Gliser <lance.gliser@torch.ai>
2023-10-10 21:05:19 -0500

    Added a mock ArangoDB connection to SystemContext

---
+++ src/config/arango.config.ts
@@ -0,0 +1,7 @@
+export const dbConfig = {
+  dbName: process.env.ARANGO_DB,
+  username: process.env.ARANGO_USER,
+  password: process.env.ARANGO_PASSWORD,
+  host: process.env.ARANGO_HOST,
+  port: process.env.ARANGO_PORT,
+};
--- src/server/systemContext.ts
+++ src/server/systemContext.ts
@@ -1,11 +1,13 @@
 import { Application } from "express";
 import { Logger } from "winston";
 import logger from "./logger";
+import { Database } from "arangojs";
+import { dbConfig } from "../config/arango.config";
 
 /** Defines the internal repos and library instances that are required during bootstrap */
 export interface SystemContext {
   logger: Logger;
-  // usersRepo: UsersRepo;
+  db: Database;
 }
 
 export const useSystemContext = (app: Application): void => {
@@ -18,9 +20,40 @@ export const useSystemContext = (app: Application): void => {
 
 /** Provides the internal repos and library instances that are required during bootstrap */
 export const getSystemContext = async (): Promise<SystemContext> => {
-  // const db = await getDb();
+  const db = await getArangoDb(dbConfig);
+
   return {
     logger,
-    // usersRepo: new UsersRepo(db),
+    db,
   };
+};
+
+export interface DBConnectionConfig {
+  dbName?: string | undefined;
+  username: string | undefined;
+  password: string | undefined;
+  host: string | undefined;
+  port: string | undefined;
+}
+
+export const getArangoDb = async (
+  config: DBConnectionConfig,
+): Promise<Database> => {
+  const { dbName = "default", username, password, host, port } = config;
+  if (!host) {
+    throw new Error("host is undefined");
+  }
+
+  const arangoConf = {
+    url: `http://${host}:${port}`,
+  };
+
+  const db = new Database(arangoConf);
+  db.useBasicAuth(username, password || "");
+
+  const dbNames = await db.listDatabases();
+  if (!dbNames.includes(dbName)) {
+    await db.createDatabase(dbName);
+  }
+  return db;
 };
